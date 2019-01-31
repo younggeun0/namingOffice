acf34c5f5f431994c607402c31fd8f3efd62b1b0
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-01-31 18:09:37 +0900

    Update View

--- src/namingOffice/view/NamingOfficeView.java
+++ src/namingOffice/view/NamingOfficeView.java
@@ -1,13 +1,27 @@
 package namingOffice.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import namingOffice.controller.NamingOfficeController;
 
 public class NamingOfficeView extends JFrame {
+	
+	JButton jbCreate, jbClose;
 	
 	public NamingOfficeView() {
 		super("Young's Naming Office");
 		
+		jbCreate = new JButton("작명하기");
+		jbClose = new JButton("종료");
 		
+		JPanel panel = new JPanel();
+		
+		NamingOfficeController noc = new NamingOfficeController(this);
+		jbCreate.addActionListener(noc);
+		jbClose.addActionListener(noc);
+		addWindowListener(noc);
 
 		setBounds(400, 300, 500, 300);
 		setVisible(true);
